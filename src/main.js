d5116573da882f5871155488d824d4f8d83bc716
ao1115 <1677120510@qq.com>
2021-12-15 15:42:15 +0800

    完成添加删除功能

--- src/main.js
+++ src/main.js
@@ -2,65 +2,84 @@ const $siteList = $(".siteList");
 const $lastLi = $(".siteList").find("li.last");
 const x = localStorage.getItem("x"); //读取当前网站下的x
 const xObject = JSON.parse(x);
-const hashMap = xObject || [
-  //如果x可以读取就读取x,否则初始化为一个含有两项的数组
-  { logo: "A", url: "http://www.acfun.cn" },
-  { logo: "B", url: "http://www.bilibili.com" },
-];
+const hashMap = xObject || []
+console.log(hashMap)
+
 
 const simplifyUrl = (url) => {
   return url
     .replace("https://", "")
     .replace("http://", "")
     .replace("www.", "")
-    .replace(/\/.*/, ""); //用正则表达式删除后面/开头的所有内容
+    .replace(/\/.*/, "") //用正则表达式删除后面/开头的所有内容
+    .replace(".com","")
+    .replace(".cn","")
 };
 
-const render = () => {
-  $siteList.find("li:not(.last)").remove();
-  hashMap.forEach((node,index) => {
-    const $li = $(`<li>
+$(".addButton").on("click", () => {
+  let url = window.prompt("请问你要访问的网址是？");
+  if(url.length ===0){
+    window.alert('网址不能为空')
+    return
+  }
+  if (url.indexOf("http") !== 0) {
+    //如果http不存在就给加上
+    url = "https://" + url;
+  }
+  const $li = $(`<li>
+        <a href = ${url} target = "true">
         <div class="site">
             <div class="logo">
-                ${node.logo}
+            <svg class="icon">
+                <use xlink:href="#icon-lianjie"></use>
+            </svg>
             </div>
             <div class="close">
                 <svg class="icon">
                      <use xlink:href="#icon-close"></use>
                 </svg>
             </div>
-            <div class="link">${simplifyUrl(node.url)}</div>
+            <div class="link" class= "addLink">${simplifyUrl(url)}</div>
         </div>
+        </a>
     </li>`).insertBefore($lastLi);
-    $li.on('click', () => {
-        window.open(node.url)
+    hashMap.push({
+      url:url
     })
-    
-    $li.on('click', '.close',(e)=> {
-        e.stopPropagation() //阻止冒泡
-        hashMap.splice(index, 1)
-        render()
-    })    
-  });
-};
-
-
-render();
-
-$(".addButton").on("click", () => {
-  let url = window.prompt("请问你要访问的网址是？");
-  if (url.indexOf("http") !== 0) {
-    //如果http不存在就给加上
-    url = "https://" + url;
-  }
-  hashMap.push({
-    logo: simplifyUrl(url)[0].toUpperCase(),
-    url: url,
-  });
-  render();
+    const string = JSON.stringify(hashMap); //只能存字符串，所以先转为字符串
+    localStorage.setItem("x", string); //在本地的x里面存入哈希表
 });
-window.onbeforeunload = () => {
+
+hashMap.forEach((node,index)=>{
+  const $li = $(`<li>
+        <a href = ${node.url} target = "true">
+        <div class="site">
+            <div class="logo">
+            <svg class="icon">
+                <use xlink:href="#icon-lianjie"></use>
+            </svg>
+            </div>
+            <div class="close">
+                <svg class="icon">
+                     <use xlink:href="#icon-close"></use>
+                </svg>
+            </div>
+            <div class="link" class= "addLink">${simplifyUrl(node.url)}</div>
+        </div>
+        </a>
+    </li>`).insertBefore($lastLi);
+    
+    $('li').on('click', '.close',(e)=> {
+      e.preventDefault() //阻止冒泡
+        e.delegateTarget.remove()
+        hashMap.splice(index,1)
+      })
+    })
+
+
+window.onbeforeunload= () => {
   //自动保存
+  console.log(1)
   const string = JSON.stringify(hashMap); //只能存字符串，所以先转为字符串
   localStorage.setItem("x", string); //在本地的x里面存入哈希表
 };
@@ -72,4 +91,4 @@ $(document).on('keypress', (e) => {
             window.open(hashMap[i].url)
         }
     }
-})
+  })
